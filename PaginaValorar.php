851a2a917a6198eea4ed95c8f1ee37d215859275
Lucia195 <luciaaa96@educastur.es>
2025-11-22 13:16:04 +0100

    InsertarValoracion

--- PaginaValorar.php
+++ PaginaValorar.php
@@ -1,10 +1,17 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once "ParquesDAO.class.php";
 require_once "ConsultasDAO.class.php";
 require_once "Restaurante.class.php";
 require_once "ParqueAtracciones.class.php";
 require_once "InsercionesDAO.class.php";
+require_once "Usuario.class.php";
+
 session_start();
+
 
 //Recuperamos el id y el tipo de manera segura
 $parque_id = filter_input(INPUT_POST, 'parque_id', FILTER_VALIDATE_INT);
@@ -19,10 +26,20 @@ $restaurantes = ConsultasDAO::getRestaurantesPorParque($parque_id);
 $mensaje_exito = '';
 $errores = [];
 
-//Aquí tendría que llamar al método para insertar la valoración de restaurante
 if (isset($_POST['valoracion_restaurante'])) {
+    $usuario_id = $_SESSION['usuario']->getIdUsuario() ?? null;
+    $exito = InsercionesDAO::valoracion($usuario_id, $_POST['restaurante_id'], "restaurante", $_POST['puntuacion'], $_POST['comentario']);
+}
+
+if (isset($_POST['valoracion-atraccion'])) {
     //Esto no está funcionando, pendiente para arreglar
-    $exito = InsercionesDAO::valoracion($_SESSION['usuario']->id, $_POST['restaurante_id'], "restaurante", $_POST['puntuacion'], $_POST['comentario']);
+    $usuario_id = $_SESSION['usuario']->getIdUsuario() ?? null;
+    $exito = InsercionesDAO::valoracion($usuario_id, $_POST['atraccion_id'], "atraccion", $_POST['puntuacion-atraccion'], $_POST['comentario-atraccion']);
+}
+if (isset($_POST['valoracion-zona'])) {
+    //Esto no está funcionando, pendiente para arreglar
+    $usuario_id = $_SESSION['usuario']->getIdUsuario() ?? null;
+    $exito = InsercionesDAO::valoracion($usuario_id, $_POST['zona_publica_id'], "zona_publica", $_POST['puntuacion-zona'], $_POST['comentario-zona']);
 }
 
 $atracciones = ParquesDAO::getAtracciones($parque_id);
@@ -70,6 +87,7 @@ $zonas = ParquesDAO::getZonas($parque_id);
             <input type="number" name="puntuacion" id="puntuacion" required min="0" max="5" step="1" value="<?php echo htmlspecialchars($puntuacion_seleccionada); ?>"placeholder="Introduce un número del 0 al 5">
             <label for="comentario">Descripcion:</label>
             <textarea name="comentario" id="comentario" required></textarea>
+            <input type="hidden" name="parque_id" value="<?php echo htmlspecialchars($parque_id); ?>">
             <button type="submit" name="valoracion_restaurante" id="valoracion_restaurante">Enviar valoracion</button>
         </form>
 
@@ -98,6 +116,7 @@ $zonas = ParquesDAO::getZonas($parque_id);
             <input type="number" name="puntuacion-atraccion" id="puntuacion-atraccion" required min="0" max="5" step="1" value="<?php echo htmlspecialchars($puntuacion_seleccionada); ?>"placeholder="Introduce un número del 0 al 5">
             <label for="comentario-atraccion">Descripcion:</label>
             <textarea name="comentario-atraccion" id="comentario-atraccion"></textarea>
+            <input type="hidden" name="parque_id" value="<?php echo htmlspecialchars($parque_id); ?>">
             <button type="submit" name="valoracion-atraccion" id="valoracion-atraccion">Enviar valoracion</button>
         </form>
 
@@ -126,6 +145,7 @@ $zonas = ParquesDAO::getZonas($parque_id);
             <input type="number" name="puntuacion-zona" id="puntuacion-zona" required min="0" max="5" step="1" value="<?php echo htmlspecialchars($puntuacion_seleccionada); ?>"placeholder="Introduce un número del 0 al 5">
             <label for="comentario-zona">Descripcion:</label>
             <textarea name="comentario-zona" id="comentario-zona"></textarea>
+            <input type="hidden" name="parque_id" value="<?php echo htmlspecialchars($parque_id); ?>">
             <button type="submit" name="valoracion-zona" id="valoracion-zona">Enviar valoracion</button>
         </form>
     </div>
